5834770cc4fe00506f478637e92039e8ce1dacb5
silve <silverstand@hotmail.com>
2019-11-22 16:47:50 -0300

    Dadas las variables nombre, edad, salario, carnet. Seleccionar el tipo de variable ideal e imprimir por consola el resultado.

---
+++ src/ejercicio1.java
@@ -0,0 +1,21 @@
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre;
+		byte edad;
+		double salario;
+		boolean carnet;
+		nombre=  "Ana";
+		edad=35;
+		salario=15000;
+		carnet=true;
+		
+		System.out.println(nombre);
+		System.out.println(edad);
+		System.out.println(salario);
+		System.out.println(carnet);
+	}
+
+}
